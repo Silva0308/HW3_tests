5fa375f0f305eeeb826ca585cae53d9a5a735e70
Silva0308 <v.travinina@gmail.com>
2023-10-19 09:49:13 +0300

    HW 3 done

--- src/main/java/org/example/HWMeth.java
+++ src/main/java/org/example/HWMeth.java
@@ -1,2 +1,18 @@
-package org.example;public class HWMeth {
+package org.example;
+
+public class HWMeth {
+    public boolean evenOddNumber(int n) {
+        if (n % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean numberInInterval(int n) {
+        if (n > 25 && n < 100)
+            return true;
+        else
+            return false;
+    }
+
 }
--- src/test/java/HWMethTests.java
+++ src/test/java/HWMethTests.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class HWMethTests {
+import org.example.HWMeth;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class HWMethTests {
+    private HWMeth hw;
+    @BeforeEach
+    void initService() {
+        HWMeth hw = new HWMeth();
+        this.hw = hw;
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {2,3})
+    void EvenOddNumberTest(int num) {
+        if(num == 2)
+            assert hw.evenOddNumber(num) == true;
+        else if(num == 3)
+            assert hw.evenOddNumber(num) == false;
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10,30,120})
+    void numberInIntervalTest(int num) {
+        if(num < 25)
+            assert hw.numberInInterval(num) == false;
+        else if(num > 100)
+            assert hw.numberInInterval(num) == false;
+        else
+            assert hw.numberInInterval(num) == true;
+    }
 }
